c005708ea7720a45b5cde7048857531a2d7c4b24
ilBuso <69167620+ilBuso@users.noreply.github.com>
2024-01-27 19:28:44 +0100

    Deploy??

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ['ts', 'tsx'],
+
+    env: {
+        MONGO_URL: "mongodb+srv://ilbuso:oSINQeCJP9FYDGds@bookeasy-0.zxqse2p.mongodb.net/?retryWrites=true&w=majority",
+    }
 }
 module.exports = nextConfig
